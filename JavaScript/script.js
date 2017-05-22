fa37fdbb80c9fe60857f426fde18b734d0c3a8ae
Yehoshua Morales <yehoshua0702@gmail.com>
2017-05-22 05:22:31 +0000

    Space Invaders

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,5 +1,21 @@
 // Helper functions
+function AABBIntersect(ax, ay, aw, ah, bx, by, bw, bh) {
+    return ax < bx + bw && bx < ax + aw && ay < by + bh && by < ay + ah;
+}
 
+//Bullets
+function Bullet(x, y, vely, w, h, color) {
+    this.x = x;
+    this.y = y;
+    this.vely = vely;
+    this.width = w;
+    this.height = h;
+    this.color = color;
+};
+
+Bullet.prototype.update = function () {
+    this.y += this.vely;
+}
 
 // Screen
 function Screen(width, height) {
@@ -11,8 +27,17 @@ function Screen(width, height) {
     document.body.appendChild(this.canvas);
 };
 
+Screen.prototype.clear = function () {
+    this.ctx.clearRect(0, 0, this.width, this.height);
+}
+
 Screen.prototype.drawSprite = function (sp, x, y) {
     this.ctx.drawImage(sp.img, sp.x, sp.y, sp.w, sp.h, x, y, sp.w, sp.h);
+};
+
+Screen.prototype.drawBullet = function (bullet) {
+    this.ctx.fillStyle = bullet.color;
+    this.ctx.fillRect(bullet.x, bullet.y, bullet.width, bullet.height);
 };
 
 // Sprite
